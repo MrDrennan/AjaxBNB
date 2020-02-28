bc1b4e32dd350a6c49d4e4b8ab6cd5340b828df4
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-02-28 13:42:33 -0500

    Added dictionary.php and dictionary.html

---
+++ dictionary.php
@@ -0,0 +1,12 @@
+<?php
+
+$terms = array(
+    'ajax'=>"Asynchronous JavaScript and XML. Ajax is a new technique for creating better, faster, 
+    and more interactive web applications with the help of XML, HTML, CSS, and Java Script.",
+    'button'=>"a small disk, knob, or the like for sewing or otherwise attaching to an article, 
+    as of clothing, serving as a fastening when passed through a buttonhole or loop.",
+    'database'=>'An organized collection of data, generally stored and accessed electronically from a computer system.',
+    'load'=>'a heavy or bulky thing that is being carried or is about to be carried',
+    'pdo'=>'PDO is an acronym for PHP Data Objects. PDO is a lean, consistent way to access databases.',
+    'peanut'=>'is Yummy! Can be used to make Peanut Butter'
+);
